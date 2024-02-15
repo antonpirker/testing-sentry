d88a9a5041b8a9036f91cd1400f176b36f544df5
Anton Pirker <anton.pirker@sentry.io>
2024-02-15 14:26:21 +0100

    cleanup

--- test-django-celery-beat-timezone/mysite/polls/views.py
+++ test-django-celery-beat-timezone/mysite/polls/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
+
 @csrf_exempt
 def index(request):
-    1/0
-    return HttpResponse("Hello, world. You're at the polls index. Body: %s" % request.body)
+    return HttpResponse(
+        "Hello, world. You're at the polls index. Body: %s" % request.body
+    )
