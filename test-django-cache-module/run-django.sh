6858cb8684df959bb0ce6dccadf80f3fdd0e3b3c
Anton Pirker <anton.pirker@sentry.io>
2024-05-08 14:12:04 +0200

    Load testing setup

--- test-django-cache-module/run-django.sh
+++ test-django-cache-module/run-django.sh
@@ -11,11 +11,15 @@ source .venv/bin/activate
 python -m pip install -r requirements.txt
 
 pkill redis-server || true
+sleep 1
 rm -rf dump.rdb
 redis-server --daemonize yes
 
 # run migrations
 cd mysite && ./manage.py migrate && cd ..
 
-# Run Django application on localhost:8000
-cd mysite && ./manage.py runserver 0.0.0.0:8000 && cd ..
+# Run Django application on localhost:8000 (DEBUG)
+# cd mysite && ./manage.py runserver 0.0.0.0:8000 && cd ..
+
+# Run Django application on localhost:8000 (PRODUCTION)
+cd mysite && mprof run --multiprocess --output "../mprofile_$(date +%Y%m%d%H%M%S).dat" gunicorn wsgi && cd ..
