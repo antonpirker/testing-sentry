629bf3541fabd1c3ddd4ec4f6dcb28e887bf9d31
Anton Pirker <anton.pirker@sentry.io>
2024-07-19 09:06:36 +0200

    updated fastapi middlewar

--- test-fastapi-middleware/run.sh
+++ test-fastapi-middleware/run.sh
@@ -10,6 +10,6 @@ source .venv/bin/activate
 pip install -r requirements.txt
 
 # uvicorn main:app --port 5000 --reload --root-path /api/v1 &
-uvicorn main:app --port 5000 --reload
-
+# uvicorn main:app --port 5000 --reload
+uvicorn app:app --port 5000 --reload
 #nginx -c "$(pwd)/nginx.conf" 
