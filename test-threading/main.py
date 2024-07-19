bb51028ada3cdfb7f45d30f5a21f85f318707081
Anton Pirker <anton.pirker@sentry.io>
2024-07-19 10:16:34 +0200

    updated threading

--- test-threading/main.py
+++ test-threading/main.py
@@ -1,19 +1,23 @@
 import os
+import random
 import threading
+import time
 
 import sentry_sdk
 
 
-
 @sentry_sdk.tracing.trace
-def my_function():
+def my_function(id):
     print("Function: starting")
-    ...
+    time.sleep(random.randint(0,3))
+    sentry_sdk.add_breadcrumb(message=f"my_function({id}): doing something", category="function", level="info")
+    
 
-
-def thread_function():
+def thread_function(id):
     print("Thread: starting")
-    my_function()
+    time.sleep(random.randint(0,2))
+    sentry_sdk.add_breadcrumb(message=f"thread_function({id}): doing something", category="function", level="info")
+    my_function(id)
 
 
 if __name__ == "__main__":
@@ -28,8 +32,21 @@ if __name__ == "__main__":
     )   
 
     with sentry_sdk.start_transaction(op="transaction", name="main-transaction"):
-        t1 = threading.Thread(target=thread_function, daemon=True)   
-        t1.start()
-        t1.join()
+        sentry_sdk.add_breadcrumb(message=f"main(): starting threads", category="function", level="info")
+        threads = []
 
+        for i in range(5):
+            t = threading.Thread(target=thread_function, name=f"mythread{i}", args=(i, ), daemon=True)   
+            threads.append(t)
+
+            t.start()
+            sentry_sdk.add_breadcrumb(message=f"main(): thread {i} started", category="function", level="info")
+
+        for t in threads:
+            t.join()
+            sentry_sdk.add_breadcrumb(message=f"main(): thread {t} joined", category="function", level="info")
+
+        sentry_sdk.add_breadcrumb(message=f"main(): all threads joined", category="function", level="info")
+
+    raise ValueError("Last minute error")
     print("Main: all done")
