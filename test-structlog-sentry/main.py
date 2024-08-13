c18ca5be77500e57467d32e48108bd335da99c1a
Anton Pirker <anton.pirker@sentry.io>
2024-08-13 12:12:49 +0200

    Fixed structlog

--- test-structlog-sentry/main.py
+++ test-structlog-sentry/main.py
@@ -9,8 +9,8 @@ from structlog_sentry import SentryProcessor
 def main():
     sentry_sdk.init(
         dsn=os.environ.get("SENTRY_DSN"),
-        # traces_sample_rate=1.0,
-        # debug=True,
+        traces_sample_rate=1.0,
+        debug=True,
     )
 
     structlog.configure(
@@ -19,6 +19,7 @@ def main():
             structlog.stdlib.add_logger_name,  # optional, must be placed before SentryProcessor()
             structlog.stdlib.add_log_level,  # required before SentryProcessor()
             SentryProcessor(event_level=logging.ERROR),
+            structlog.stdlib.ProcessorFormatter.wrap_for_formatter,
         ],
         logger_factory=structlog.stdlib.LoggerFactory(),
         wrapper_class=structlog.stdlib.BoundLogger,
