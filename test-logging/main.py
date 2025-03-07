e06d8a291d4f86263bc122eb1d8d5449b3d19504
Anton Pirker <anton.pirker@sentry.io>
2025-03-07 10:35:40 +0100

    log messages

--- test-logging/main.py
+++ test-logging/main.py
@@ -17,32 +17,22 @@ async def main():
         traces_sample_rate=1.0,
         send_default_pii=True,
         debug=True,
-
     )
 
     string_value = "SomeStringValue"
     non_string_value = 42
     none_value = None
 
-
     with sentry_sdk.start_transaction(op="function", name="logging-transaction"):
-        # logger.warning("warn1 %s", "None")
-        logger.warning("warnXX %s", None)
-        # logger.warning("warn3 %s", 3)
+        logger.debug("I am ignored [%s] [%s] [%s]", string_value, non_string_value, none_value)
+        logger.info("I am a breadcrumb [%s] [%s] [%s]", string_value, non_string_value, none_value)
+        logger.error("I am an event [%s] [%s] [%s]", string_value, non_string_value, none_value)
+        logger.exception("An exception happened [%s] [%s] [%s]", string_value, non_string_value, none_value)
 
-        # logger.error("error1 %s", "None")
-        logger.error("errorXX %s", None)
-        # logger.error("error3 %s", 3)
-
-        # logger.debug("I am ignored [%s] [%s] [%s]", string_value, non_string_value, none_value)
-        # logger.info("I am a breadcrumb [%s] [%s] [%s]", string_value, non_string_value, none_value)
-        # logger.error("I am an event [%s] [%s] [%s]", string_value, non_string_value, none_value)
-        # logger.exception("An exception happened [%s] [%s] [%s]", string_value, non_string_value, none_value)
-
-        # logger.debug(f"[f-string] I am ignored [{string_value}] [{non_string_value}] [{none_value}]")
-        # logger.info(f"[f-string] I am a breadcrumb [{string_value}] [{non_string_value}] [{none_value}]")
-        # logger.error(f"[f-string] I am an event [{string_value}] [{non_string_value}] [{none_value}]")
-        # logger.exception(f"[f-string] An exception happened [{string_value}] [{non_string_value}] [{none_value}]")
+        logger.debug(f"[f-string] I am ignored [{string_value}] [{non_string_value}] [{none_value}]")
+        logger.info(f"[f-string] I am a breadcrumb [{string_value}] [{non_string_value}] [{none_value}]")
+        logger.error(f"[f-string] I am an event [{string_value}] [{non_string_value}] [{none_value}]")
+        logger.exception(f"[f-string] An exception happened [{string_value}] [{non_string_value}] [{none_value}]")
 
 
 if __name__ == "__main__":
