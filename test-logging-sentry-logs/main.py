9bee462aff9b28156902217dbdbe935c4e8749fb
Anton Pirker <anton.pirker@sentry.io>
2025-03-27 09:48:39 +0100

    more logging

--- test-logging-sentry-logs/main.py
+++ test-logging-sentry-logs/main.py
@@ -3,6 +3,7 @@ import os
 
 import sentry_sdk
 from sentry_sdk.integrations.logging import LoggingIntegration
+import sentry_sdk._experimental_logger as sentry_logger
 
 import logging
 logger = logging.getLogger()
@@ -20,6 +21,7 @@ async def main():
             LoggingIntegration(
                 level=logging.DEBUG,
                 event_level=logging.ERROR,
+                sentry_logs_level=logging.WARNING,
             ),
         ],
         _experiments={
@@ -33,6 +35,10 @@ async def main():
     my_value = "SomeStringValue"
 
     with sentry_sdk.start_transaction(op="function", name="logging-transaction"):
+        sentry_logger.debug("Calling API directly. Thats the status: {my_status} / and the value: {my_value}", my_status=200, my_value="some value")
+
+        logger.critical("Using integration and setting stack_info, stacklevel, and extra[my_status, my_value]. %s", my_value, stack_info=True, stacklevel=2, extra={"my_status": 200, "my_value": "some value"})
+
         logger.debug("Using python.logging.debug, my_value: %s", my_value)
         logger.info("Using python.logging.info, my_value: %s", my_value)
         logger.warning("Using python.logging.warning, my_value: %s", my_value)
@@ -40,12 +46,12 @@ async def main():
         logger.critical("Using python.logging.critical, my_value: %s", my_value)
         logger.exception("Using python.logging.exception, my_value: %s", my_value)
 
-        logger.debug(f"Using python.logging.debug with f-string, my_value: {my_value}")
-        logger.info(f"Using python.logging.info with f-string, my_value: {my_value}")
-        logger.warning(f"Using python.logging.warning with f-string, my_value: {my_value}")
-        logger.error(f"Using python.logging.error with f-string, my_value: {my_value}")
-        logger.critical(f"Using python.logging.critical with f-string, my_value: {my_value}")
-        logger.exception(f"Using python.logging.exception with f-string, my_value: {my_value}")
+        logger.debug(f"[f-string version] Using python.logging.debug, my_value: {my_value}")
+        logger.info(f"[f-string version] Using python.logging.info, my_value: {my_value}")
+        logger.warning(f"[f-string version] Using python.logging.warning, my_value: {my_value}")
+        logger.error(f"[f-string version] Using python.logging.error, my_value: {my_value}")
+        logger.critical(f"[f-string version] Using python.logging.critical, my_value: {my_value}")
+        logger.exception(f"[f-string version] Using python.logging.exception, my_value: {my_value}")
 
 
 if __name__ == "__main__":
