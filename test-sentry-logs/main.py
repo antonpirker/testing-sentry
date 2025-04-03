44179b94103c1caaac0970610f4fff18037c4fb7
Anton Pirker <anton.pirker@sentry.io>
2025-04-03 11:51:59 +0200

    updated logging stuff

--- test-sentry-logs/main.py
+++ test-sentry-logs/main.py
@@ -2,17 +2,13 @@ import os
 import random
 
 import sentry_sdk
-
-from sentry_sdk import _experimental_logger as sentry_logger
+import sentry_sdk.logger
 
 
 def before_log(record, hint):
-    print("before_log record", record)
-    print("before_log hint", hint)
-
     if record['severity_text'] == 'fatal':
         return None
-    
+
     return record
 
 
@@ -20,23 +16,23 @@ sentry_sdk.init(
     dsn=os.environ.get("SENTRY_DSN"),
     environment=os.environ.get("ENV", "test"),
     traces_sample_rate=1.0,
-    profiles_sample_rate=1.0,
     debug=True,
-    enable_sentry_logs=True,
-    before_emit_log=before_log,
+    _experiments={
+        "enable_logs": True,
+        "before_send_log": before_log,
+    },
 )
 
 
 def main():
     for x in range(1000):
-        sentry_logger.trace("This is a 'trace' log...{rand}", rand=random.randint(0, 100)) 
-        sentry_logger.debug("This is a 'debug' log... ({rand})", rand=random.randint(0, 100), bla="asdfoamsdfl") 
-        sentry_logger.info("This is a 'info' log... ({rand})", rand=random.randint(0, 100)) 
-        sentry_logger.warn("This is a 'warn' log... ({rand})", rand=random.randint(0, 100)) 
-        sentry_logger.error("This is a 'error' log... ({rand})", rand=random.randint(0, 100)) 
-        sentry_logger.fatal("This is a 'fatal' log... ({rand})", rand=random.randint(0, 100)) 
+        sentry_sdk.logger.trace("This is a 'trace' log...{rand}", rand=random.randint(0, 100))
+        sentry_sdk.logger.debug("This is a 'debug' log... ({rand})", rand=random.randint(0, 100), bla="asdfoamsdfl")
+        sentry_sdk.logger.info("This is a 'info' log... ({rand})", rand=random.randint(0, 100))
+        sentry_sdk.logger.warning("This is a 'warn' log... ({rand})", rand=random.randint(0, 100))
+        sentry_sdk.logger.error("This is a 'error' log... ({rand})", rand=random.randint(0, 100))
+        sentry_sdk.logger.fatal("This is a 'fatal' log... ({rand})", rand=random.randint(0, 100))
 
 
-# For running directly
 if __name__ == '__main__':
     main()
