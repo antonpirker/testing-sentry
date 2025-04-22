0380f9a2f10fb50e6d6f18734c38741ffd80085e
Anton Pirker <anton.pirker@sentry.io>
2025-04-22 16:03:50 +0200

    more data

--- test-potel-fastapi/main.py
+++ test-potel-fastapi/main.py
@@ -1,7 +1,14 @@
-from fastapi import FastAPI, Depends
 import os
+import time
+
+from fastapi import FastAPI, Depends
 
 import sentry_sdk
+from sentry_sdk import logger as sentry_logger
+from sentry_sdk.crons import monitor
+from sentry_sdk.types import MonitorConfig
+from sentry_sdk.feature_flags import add_feature_flag
+
 sentry_sdk.init(
     dsn=os.environ.get("SENTRY_DSN"),
     environment=os.environ.get("ENV", "fastapi"),
@@ -9,10 +16,25 @@ sentry_sdk.init(
     traces_sample_rate=1.0,
     profiles_sample_rate=1.0,
     debug=True,
+    _experiments={
+        "enable_logs": True,
+    },
 )
 
 
 app = FastAPI()
+
+def _fibonacci(n):
+    print(f"fibonacci {n}")
+    time.sleep(0.05)
+    if n < 0:
+        print("Incorrect input")
+    elif n == 0:
+        return 0
+    elif n == 1 or n == 2:
+        return 1
+    else:
+        return _fibonacci(n-1) + _fibonacci(n-2)
 
 
 def get_user():
@@ -30,10 +52,33 @@ async def test_middleware(request, call_next):
     return await call_next(request)
 
 
+monitor_config: MonitorConfig = {
+    "schedule": {"type": "crontab", "value": "0 0 * * *"},
+    "timezone": "Europe/Vienna",
+    # If an expected check-in doesn't come in `checkin_margin`
+    # minutes, it'll be considered missed
+    "checkin_margin": 10,
+    # The check-in is allowed to run for `max_runtime` minutes
+    # before it's considered failed
+    "max_runtime": 10,
+    # It'll take `failure_issue_threshold` consecutive failed
+    # check-ins to create an issue
+    "failure_issue_threshold": 5,
+    # It'll take `recovery_threshold` OK check-ins to resolve
+    # an issue
+    "recovery_threshold": 5,
+}
+
 @app.get("/error")
+@monitor(monitor_slug="test-cron", monitor_config=monitor_config)
 def error(user=Depends(get_user)):
+    # feature flag
+    add_feature_flag("test-flag", True)
+
+    # spans
     with sentry_sdk.start_span(name="test-span"):
-        with sentry_sdk.start_span(name="test-span-2") as span:
+        with sentry_sdk.start_span(name="test-span-2"):
+
             # tag
             sentry_sdk.set_tag("test-tag", "test-value")
 
@@ -55,6 +100,13 @@ def error(user=Depends(get_user)):
             scope = sentry_sdk.get_current_scope()
             scope.add_attachment(bytes=b"Hello World", filename="hello.txt")
 
+            # logs
+            sentry_logger.warning("test log")
+
+            # do some work, so we have a profile
+            _fibonacci(5)
+
+            # raise an error
             raise ValueError("help! an error!")
 
 
