8a6fc36734ad4eba5364ce42aede5893a57fce0d
Anton Pirker <anton.pirker@sentry.io>
2025-04-22 15:22:57 +0200

    transaction and span with more data

--- test-potel-fastapi/main.py
+++ test-potel-fastapi/main.py
@@ -2,23 +2,13 @@ from fastapi import FastAPI, Depends
 import os
 
 import sentry_sdk
-from sentry_sdk.integrations.starlette import StarletteIntegration
-from sentry_sdk.integrations.fastapi import FastApiIntegration
-
-
-failed_request_status_codes = [range(400, 600)]
-
-
 sentry_sdk.init(
     dsn=os.environ.get("SENTRY_DSN"),
-    environment=os.environ.get("ENV", "test"),
+    environment=os.environ.get("ENV", "fastapi"),
+    release=os.environ.get("RELEASE", "fastapi@0.0.1a"),
     traces_sample_rate=1.0,
     profiles_sample_rate=1.0,
     debug=True,
-    integrations=[
-        StarletteIntegration(failed_request_status_codes=failed_request_status_codes),
-        FastApiIntegration(failed_request_status_codes=failed_request_status_codes),
-    ],
 )
 
 
@@ -27,7 +17,8 @@ app = FastAPI()
 
 def get_user():
     user = {
-      "id": "testuser"
+      "id": "testuser",
+      "username": "Test User",
     }
     sentry_sdk.set_user(user)
     return user
@@ -41,12 +32,30 @@ async def test_middleware(request, call_next):
 
 @app.get("/error")
 def error(user=Depends(get_user)):
-    raise ValueError("help! an error!")
+    with sentry_sdk.start_span(name="test-span"):
+        with sentry_sdk.start_span(name="test-span-2") as span:
+            # tag
+            sentry_sdk.set_tag("test-tag", "test-value")
 
+            # user
+            sentry_sdk.set_user(user)
 
-@app.post("/post")
-def post(user):
-    return user
+            # breadcrumb
+            sentry_sdk.add_breadcrumb(
+                category="test-category",
+                message="test breadcrumb",
+                level="info",
+                data={"user": user},
+            )
+
+            # context
+            sentry_sdk.set_context("test-context", {"text-context-user": user})
+
+            # attachment
+            scope = sentry_sdk.get_current_scope()
+            scope.add_attachment(bytes=b"Hello World", filename="hello.txt")
+
+            raise ValueError("help! an error!")
 
 
 @app.get("/")
