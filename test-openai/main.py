c222bd4657648544441309eebe9a3e1a1ed6c65a
Anton Pirker <anton.pirker@sentry.io>
2025-07-21 15:24:57 +0200

    updated openai

--- test-openai/main.py
+++ test-openai/main.py
@@ -7,8 +7,8 @@ from sentry_sdk.ai.monitoring import ai_track
 from sentry_sdk.integrations.openai import OpenAIIntegration
 
 
-@ai_track("My sync OpenAI pipeline")
-def my_pipeline(client):
+@ai_track("My sync OpenAI workflow")
+def my_workflow(client):
     with sentry_sdk.start_transaction(name="openai-sync"):
         # Responses API
         response = client.responses.create(
@@ -16,6 +16,7 @@ def my_pipeline(client):
             instructions="You are a coding assistant that talks like a pirate.",
             input="How do I check if a Python object is an instance of a class?",
         )
+        print("--------------------------------")
         print("Response:")
         print(response.model_dump())
 
@@ -29,6 +30,7 @@ def my_pipeline(client):
             ],
             model="gpt-3.5-turbo",
         )
+        print("--------------------------------")
         print("Message:")
         print(message.dict())
 
@@ -37,6 +39,7 @@ def my_pipeline(client):
             input="Your text goes here",
             model="text-embedding-3-small",
         ).data[0].embedding
+        print("--------------------------------")
         print("Embeddings:")
         print(len(embeddings))
 
@@ -44,7 +47,7 @@ def my_pipeline(client):
 def main():
     sentry_sdk.init(
         dsn=os.getenv("SENTRY_DSN", None),
-        environment=os.getenv("ENV", "local"),
+        environment=os.getenv("ENV", "openai-test-sync"),
         traces_sample_rate=1.0,
         send_default_pii=True,
         debug=True,
@@ -57,7 +60,7 @@ def main():
         api_key=os.environ.get("OPENAI_API_KEY"),
     )
 
-    my_pipeline(client)
+    my_workflow(client)
 
 
 if __name__ == "__main__":
--- test-openai/main_async.py
+++ test-openai/main_async.py
@@ -8,10 +8,20 @@ from sentry_sdk.ai.monitoring import ai_track
 from sentry_sdk.integrations.openai import OpenAIIntegration
 
 
-@ai_track("My async OpenAI pipeline")
-async def my_pipeline(client):
+@ai_track("My async OpenAI workflow")
+async def my_workflow(client):
     with sentry_sdk.start_transaction(name="openai-async"):
-        # Async create message
+        # Responses API
+        response = await client.responses.create(
+            model="gpt-4o-mini",
+            instructions="You are a coding assistant that talks like a pirate.",
+            input="How do I check if a Python object is an instance of a class?",
+        )
+        print("--------------------------------")
+        print("Response:")
+        print(response.model_dump())
+
+        # Completions API
         message = await client.chat.completions.create(
             messages=[
                 {
@@ -21,39 +31,24 @@ async def my_pipeline(client):
             ],
             model="gpt-3.5-turbo",
         )
+        print("--------------------------------")
         print("Message:")
         print(message.dict())
 
-        # Async embeddings
-        result = await client.embeddings.create(
+        # Embeddings API
+        embeddings = await client.embeddings.create(
             input="Your text goes here",
             model="text-embedding-3-small",
-        )
-        embedding = result.data[0].embedding
-        print("Embedding:")
-        print(len(embedding))
-
-        # Async streams are only in beta in OpenAI
-        # # Async create streaming message
-        # stream = await client.chat.completions.create(
-        #     messages=[
-        #         {
-        #             "role": "user",
-        #             "content": "Hi!",
-        #         }
-        #     ],
-        #     model="gpt-3.5-turbo",
-        #     stream=True,
-        # )
-        # print("Message (Stream):")
-        # for event in stream:
-        #     print(event)
+        ).data[0].embedding
+        print("--------------------------------")
+        print("Embeddings:")
+        print(len(embeddings))
 
 
 async def main():
     sentry_sdk.init(
         dsn=os.getenv("SENTRY_DSN", None),
-        environment=os.getenv("ENV", "local"),
+        environment=os.getenv("ENV", "openai-test-async"),
         traces_sample_rate=1.0,
         send_default_pii=True,
         debug=True,
@@ -66,7 +61,7 @@ async def main():
         api_key=os.environ.get("OPENAI_API_KEY"),
     )
 
-    await my_pipeline(client)
+    await my_workflow(client)
 
 
 asyncio.run(main())
