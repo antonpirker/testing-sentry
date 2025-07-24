6d0f8e652d968613cbe9d7f5c2edc67357a5c75d
Anton Pirker <anton.pirker@sentry.io>
2025-07-24 11:06:18 +0200

    updated openai test proj

--- test-openai/main.py
+++ test-openai/main.py
@@ -7,19 +7,60 @@ from sentry_sdk.ai.monitoring import ai_track
 from sentry_sdk.integrations.openai import OpenAIIntegration
 from sentry_sdk.integrations.stdlib import StdlibIntegration
 
+tools = [{
+    "type": "function",
+    "name": "get_weather",
+    "description": "Get current temperature for provided coordinates in celsius.",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "latitude": {"type": "number"},
+            "longitude": {"type": "number"}
+        },
+        "required": ["latitude", "longitude"],
+        "additionalProperties": False
+    },
+    "strict": True
+}, {
+    "type": "web_search_preview",
+    "user_location": {
+        "type": "approximate",
+        "country": "GB",
+        "city": "London",
+        "region": "London"
+    }
+}]
 
 @ai_track("My sync OpenAI workflow")
 def my_workflow(client):
     with sentry_sdk.start_transaction(name="openai-sync"):
-        # Responses API
+        # Responses API with tools
         response = client.responses.create(
             model="gpt-4o-mini",
-            instructions="You are a coding assistant that talks like a pirate.",
-            input="How do I check if a Python object is an instance of a class?",
+            instructions="You are a assistant that talks like a pirate.",
+            input="What is the weather in Paris?",
+            tools=tools,
+            temperature=0.2,
+            top_p=0.3,
         )
         print("--------------------------------")
         print("Response:")
         print(response.model_dump())
+
+        # Responses API with streaming
+        response = client.responses.create(
+            model="gpt-4o-mini",
+            instructions="You are a assistant that talks like a pirate.",
+            input="What is the weather in Paris?",
+            temperature=0.2,
+            top_p=0.3,
+            stream=True,
+        )
+        print("--------------------------------")
+        print("Streaming Response:")
+        for chunk in response:
+            print("Chunk:")
+            print(chunk.model_dump())
 
         # Completions API
         message = client.chat.completions.create(
@@ -30,14 +71,18 @@ def my_workflow(client):
                 }
             ],
             model="gpt-3.5-turbo",
+            max_tokens=666,
+            presence_penalty=0.1,
+            temperature=0.2,
+            top_p=0.3,
         )
         print("--------------------------------")
         print("Message:")
-        print(message.dict())
+        print(message.model_dump())
 
         # Embeddings API
         embeddings = client.embeddings.create(
-            input="Your text goes here",
+            input="The text I want to calculate the embeddings for. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
             model="text-embedding-3-small",
         ).data[0].embedding
         print("--------------------------------")
@@ -50,10 +95,14 @@ def main():
         dsn=os.getenv("SENTRY_DSN", None),
         environment=os.getenv("ENV", "openai-test-sync"),
         traces_sample_rate=1.0,
+        profiles_sample_rate=1.0,
         send_default_pii=True,
         debug=True,
         integrations=[
-            OpenAIIntegration(include_prompts=True),
+            OpenAIIntegration(
+                include_prompts=True,
+                tiktoken_encoding_name="cl100k_base",
+            ),
         ],
         disabled_integrations=[
             StdlibIntegration(),
--- test-openai/main_async.py
+++ test-openai/main_async.py
@@ -9,18 +9,61 @@ from sentry_sdk.integrations.openai import OpenAIIntegration
 from sentry_sdk.integrations.stdlib import StdlibIntegration
 
 
+tools = [{
+    "type": "function",
+    "name": "get_weather",
+    "description": "Get current temperature for provided coordinates in celsius.",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "latitude": {"type": "number"},
+            "longitude": {"type": "number"}
+        },
+        "required": ["latitude", "longitude"],
+        "additionalProperties": False
+    },
+    "strict": True
+}, {
+    "type": "web_search_preview",
+    "user_location": {
+        "type": "approximate",
+        "country": "GB",
+        "city": "London",
+        "region": "London"
+    }
+}]
+
+
 @ai_track("My async OpenAI workflow")
 async def my_workflow(client):
     with sentry_sdk.start_transaction(name="openai-async"):
-        # Responses API
+        # Responses API with tools
         response = await client.responses.create(
             model="gpt-4o-mini",
-            instructions="You are a coding assistant that talks like a pirate.",
-            input="How do I check if a Python object is an instance of a class?",
+            instructions="You are a assistant that talks like a pirate.",
+            input="What is the weather in Paris?",
+            tools=tools,
+            temperature=0.2,
+            top_p=0.3,
         )
         print("--------------------------------")
         print("Response:")
         print(response.model_dump())
+
+        # Responses API with streaming
+        response = await client.responses.create(
+            model="gpt-4o-mini",
+            instructions="You are a assistant that talks like a pirate.",
+            input="What is the weather in Paris?",
+            temperature=0.2,
+            top_p=0.3,
+            stream=True,
+        )
+        print("--------------------------------")
+        print("Streaming Response:")
+        async for chunk in response:
+            print("Chunk:")
+            print(chunk.model_dump())
 
         # Completions API
         message = await client.chat.completions.create(
@@ -31,17 +74,20 @@ async def my_workflow(client):
                 }
             ],
             model="gpt-3.5-turbo",
+            max_tokens=666,
+            presence_penalty=0.1,
+            temperature=0.2,
+            top_p=0.3,
         )
         print("--------------------------------")
         print("Message:")
         print(message.dict())
 
         # Embeddings API
-        embeddings = await client.embeddings.create(
-            input="Your text goes here",
+        embeddings = (await client.embeddings.create(
+            input="The text I want to calculate the embeddings for. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
             model="text-embedding-3-small",
-        )
-        embeddings = embeddings.data[0].embedding
+        )).data[0].embedding
         print("--------------------------------")
         print("Embeddings:")
         print(len(embeddings))
@@ -52,10 +98,14 @@ async def main():
         dsn=os.getenv("SENTRY_DSN", None),
         environment=os.getenv("ENV", "openai-test-async"),
         traces_sample_rate=1.0,
+        profiles_sample_rate=1.0,
         send_default_pii=True,
         debug=True,
         integrations=[
-            OpenAIIntegration(include_prompts=True),
+            OpenAIIntegration(
+                include_prompts=True,
+                tiktoken_encoding_name="cl100k_base",
+            ),
         ],
         disabled_integrations=[
             StdlibIntegration(),
