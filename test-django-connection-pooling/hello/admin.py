ea911897dbef8e521a499a6cdc54684735b475b7
Anton Pirker <anton.pirker@sentry.io>
2025-08-06 17:28:18 +0200

    admin

--- test-django-connection-pooling/hello/admin.py
+++ test-django-connection-pooling/hello/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Greeting
 
-# Register your models here.
+
+@admin.register(Greeting)
+class GreetingAdmin(admin.ModelAdmin):
+    list_display = ('message', 'count', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('message',)
+    readonly_fields = ('created_at',)
+    ordering = ('-created_at',)
