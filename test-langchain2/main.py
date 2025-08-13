97dd5057ec0cd9bf50e4d4b6f1c0d7fc51acbb19
Anton Pirker <anton.pirker@sentry.io>
2025-08-13 11:45:41 +0200

    langchain2

---
+++ test-langchain2/main.py
@@ -0,0 +1,50 @@
+import os
+
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage, SystemMessage
+
+import sentry_sdk
+from sentry_sdk.ai.monitoring import ai_track
+from sentry_sdk.integrations.langchain import LangchainIntegration
+
+
+@ai_track("My sync LangChain AI pipeline")
+def my_pipeline(llm):
+    with sentry_sdk.start_transaction(name="langchain-sync"):
+        # Sync create message
+        messages = [
+            SystemMessage(content="You are a helpful assistant."),
+            HumanMessage(content="Hi!"),
+        ]
+        response = llm.invoke(messages)
+        print("Response:")
+        print(response.dict())
+
+        # Sync create streaming message
+        print("Response (Stream):")
+        for chunk in llm.stream(messages):
+            print(chunk.dict())
+
+
+def main():
+    sentry_sdk.init(
+        dsn=os.getenv("SENTRY_DSN", None),
+        environment=os.getenv("ENV", os.path.basename(os.getcwd())),
+        traces_sample_rate=1.0,
+        send_default_pii=True,
+        debug=True,
+        integrations=[
+            LangchainIntegration(include_prompts=True),
+        ],
+    )
+
+    llm = ChatOpenAI(
+        model="gpt-3.5-turbo",
+        api_key=os.environ.get("OPENAI_API_KEY"),
+    )
+
+    my_pipeline(llm)
+
+
+if __name__ == "__main__":
+    main()
---
+++ test-langchain2/run_tool.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# exit on first error
+set -euo pipefail
+
+# Install uv if it's not installed
+if ! command -v uv &> /dev/null; then
+    curl -LsSf https://astral.sh/uv/install.sh | sh
+fi
+
+# Run the script
+uv run python main_tool.py
