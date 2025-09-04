d89eae47d975e0b7e5b082a47facce4771847ff9
Anton Pirker <anton.pirker@sentry.io>
2025-09-04 09:35:57 +0200

    streaming

--- test-huggingface-hub-chat-completion/main.py
+++ test-huggingface-hub-chat-completion/main.py
@@ -6,25 +6,23 @@ from sentry_sdk.integrations.huggingface_hub import HuggingfaceHubIntegration
 from huggingface_hub import InferenceClient
 
 
-TOOLS = [
-        {
-            "type": "function",
-            "function": {
-                "name": "get_weather",
-                "description": "Get current temperature for a given location.",
-                "parameters": {
-                    "type": "object",
-                    "properties": {
-                        "location": {
-                            "type": "string",
-                            "description": "City and country e.g. Paris, France"
-                        }
-                    },
-                    "required": ["location"],
-                },
-            }
-        }
-]
+TOOLS = [{
+    "type": "function",
+    "function": {
+        "name": "get_weather",
+        "description": "Get current temperature for a given location.",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "location": {
+                    "type": "string",
+                    "description": "City and country e.g. Paris, France"
+                }
+            },
+            "required": ["location"],
+        },
+    }
+}]
 
 
 def main():
@@ -39,54 +37,58 @@ def main():
         ],
     )
 
-    model = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
-    provider = "featherless-ai"
+    # model = "meta-llama/Meta-Llama-3-70B-Instruct"
+    model = "Qwen/Qwen2.5-72B-Instruct"
+    # provider = "hf-inference"
 
-    client = InferenceClient(
-        token=os.getenv("HF_TOKEN", None),
-        provider=provider,
-    )
+    with sentry_sdk.start_transaction(name="huggingface-hub-chat-completion"):
+        client = InferenceClient(
+            token=os.getenv("HF_TOKEN", None),
+            # provider=provider,
+        )
 
-    response = client.chat_completion(
-        model=model,
-        messages=[
-        {
-            "role": "user",
-            "content": "What's the weather like the next 3 days in London, UK?"
-        }
-        ],
-        tools=TOOLS,
-        tool_choice="auto",
-        frequency_penalty=0.1,
-        max_tokens=50,
-        presence_penalty=0.2,
-        top_p=0.9,
-        temperature=0.2,
-        stream=False,
-    )
-    print("--------------------------------")
-    print("Output client.chat_completion:")
-    print(response)
+        response = client.chat_completion(
+            model=model,
+            messages=[
+            {
+                "role": "user",
+                "content": "What is the weather in London, UK?"
+            }
+            ],
+            tools=TOOLS,
+            tool_choice="auto",
+            frequency_penalty=0.1,
+            max_tokens=500,
+            presence_penalty=0.2,
+            top_p=0.9,
+            temperature=0.2,
+            stream=True,
+        )
+        print("--------------------------------")
+        print("Output client.chat_completion:")
+        for chunk in response:
+            print(chunk)
 
-    # OpenAI compatible API
-    response = client.chat.completions.create(
-        model=model,
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": "Count to 10"},
-        ],
-        tools=TOOLS,
-        tool_choice="auto",
-        frequency_penalty=0.1,
-        max_tokens=50,
-        presence_penalty=0.2,
-        top_p=0.9,
-        temperature=0.2,
-        stream=False,
-    )
-    print("--------------------------------")
-    print("Output client.chat.completions.create:")
-    print(response)
+        # OpenAI compatible API
+        response = client.chat.completions.create(
+            model=model,
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": "Should i be nice to my partner today? Short answer yes or no."},
+            ],
+            tools=TOOLS,
+            tool_choice="auto",
+            frequency_penalty=0.1,
+            max_tokens=50,
+            presence_penalty=0.2,
+            top_p=0.9,
+            temperature=0.2,
+            stream=True,
+        )
+        print("--------------------------------")
+        print("Output client.chat.completions.create:")
+        for chunk in response:
+            print(chunk)
 
 
 if __name__ == "__main__":
